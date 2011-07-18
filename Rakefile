0ffc6d41de83b8c13f057ef0e8f1fcf8bed2637f
Jorge Manrubia <jorge.manrubia@gmail.com>
2011-07-18 19:05:14 +0200

    changed footer and rakefile for deployment

---
+++ Rakefile
@@ -0,0 +1,5 @@
+namespace :blog do
+  task :deploy do
+    sh "rsync -avz zendone-theme jorgeman@jorgemanrubia.net:/home/jorgeman/public_html/zendone.com/blog/wp-content/themes"
+  end
+end
--- zendone-theme/footer.php
+++ zendone-theme/footer.php
@@ -1,5 +1,5 @@
 	<div id="footer">
-		<p>zendone <?php bloginfo('name'); ?> by plaindrops. Theme design based in <a href="http://www.vostoktheme.com" hreflang="en">Vostok Theme</a></p>
+		<p><?php bloginfo('name'); ?> by plaindrops. Theme design based in <a href="http://www.vostoktheme.com" hreflang="en">Vostok Theme</a></p>
 	</div><!-- close:footer -->
 </div><!-- close:wrapper -->
 <?php wp_footer(); ?>
