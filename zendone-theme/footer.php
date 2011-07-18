ed4f70c27f8bee1c4825e9f2123414dd0e638281
Jorge Manrubia <jorge.manrubia@gmail.com>
2011-07-18 19:11:21 +0200

    Footer

--- zendone-theme/footer.php
+++ zendone-theme/footer.php
@@ -1,5 +1,5 @@
 	<div id="footer">
-		<p>©plaindrops <?php echo date("Y") ?>. Theme design based in <a href="http://www.vostoktheme.com" hreflang="en">Vostok Theme</a></p>
+		<p>©plaindrops <?php echo date("Y") ?>. Theme layout based in <a href="http://www.vostoktheme.com" hreflang="en">Vostok Theme</a></p>
 	</div><!-- close:footer -->
 </div><!-- close:wrapper -->
 <?php wp_footer(); ?>
