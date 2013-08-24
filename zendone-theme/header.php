bc0cde581848c5722030a3131bfb98af17cbfcae
Jorge Manrubia <jorge.manrubia@gmail.com>
2013-08-24 16:05:03 +0200

    adding google analytics

--- zendone-theme/header.php
+++ zendone-theme/header.php
@@ -24,19 +24,11 @@
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php bloginfo('name'); ?> <?php _e('Comments RSS Feed','vostok'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	
-	<script type="text/javascript">
+<script type="text/javascript">
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-24616447-2']);
-      _gaq.push(['_trackPageview']);
+var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-24616447-1']); _gaq.push(['_setDomainName', 'zendone.com']); _gaq.push(['_setAllowLinker', true]); _gaq.push(['_trackPageview']);  (function () { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();  
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
+</script>
 	
 <?php wp_head(); ?>
 </head>
